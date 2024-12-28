78fa4b841535f7b379d11b78ee5ec7cca20f7e91
FlorentinTomas <tomy.florentin29@gmail.com>
2024-12-28 17:25:43 -0300

    avance en el curso

---
+++ js-for-react/basics/index.js
@@ -0,0 +1,40 @@
+// OBJECTS
+
+// const user = {
+//     name: 'ryan',
+//     lastname: 'perez',
+//     age: 30,
+//     addres: {
+//         country: 'Colombia',
+//         city: 'Bogota',
+//         street: 'main street 123'
+//     },
+//     friends: ['brandon', 'elena'],
+//     active: true,
+//     sendMail() { //methods
+//         return 'sending email...'
+//     }
+// }
+
+// console.log(user.addres.city)
+// console.log(user.friends)
+// console.log(user.active)
+// console.log(user.sendMail())
+
+
+
+// SHORTHAND PROPERTY NAMES
+
+// const name = "laptop"
+// const price = 3000
+
+// const newProduct = {
+//     name, // Le asigna como key y valor name
+//     price
+// }
+
+// console.log(newProduct)
+
+
+
+//
